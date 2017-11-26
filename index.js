807d2aceec2a878adb6203ae59d42ae15210bc12
Jeff Walton <jwalton89@gmail.com>
2017-11-26 23:40:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
+<<<<<<< HEAD
   return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, name) {
@@ -15,3 +16,17 @@ function holidayCountdown(holiday, days) {
 happyHolidaysTo('you')
 HappyHolidayTo("Mother's Day",'you')
 holidayCountdown('20',"Mother's Day")
+=======
+  console.log(`Happy holidays, ${name}!`)
+}
+function happyHolidayTo(holiday, name) {
+  console.log(`Happy ${holiday}, ${name}!`)
+}
+function holidayCountdown(days, holiday) {
+  console.log(`It\'s ${days} days until ${holiday}!`)
+}
+
+happyHolidaysTo('you')
+HappyHolidayTo("Mother\'s Day",'you')
+holidayCountdown('20',"Mother\'s Day")
+>>>>>>> 083b53702f5ba44e1b44f0bb71ce17a1dbf9b02b
